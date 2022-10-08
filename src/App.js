a5b61dd90388e0dd21e30c367983b900e2d9ab2b
Dharm Singh <dharmjat45@gmail.com>
2022-10-08 23:30:50 +0530

    Post request using axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 
 function App() {
   return (
     <div className="App">
       <>
-    <PostList />
+    {/* <PostList /> */}
+    <PostForm/>
       </>
     </div>
   );
