d0ec6f7f24e43a6fcbda569204198a41dff1e768
Dharm Singh <dharmjat45@gmail.com>
2022-10-08 23:13:48 +0530

    Add Error in get method

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -17,17 +17,19 @@ class PostList extends Component {
         this.setState({ posts: response.data });
       })
       .catch((error) => {
-        console.log(error);
+        console.log(error)
+        this.setState({errorMsg : 'Error retrieving data'})
       });
   }
   render() {
-    const { posts } = this.state;
+    const { posts,errorMsg } = this.state;
     return (
       <div>
         List Of Posts
         {posts.length
           ? posts.map((post) => <div key={post.id}>{post.title}</div>)
           : null}
+          {errorMsg ? <div>{errorMsg}</div> : null }
       </div>
     );
   }
