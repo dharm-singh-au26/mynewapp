b1d88c475e732c7b9e8ed2f17f17c40c93871d5f
Dharm Singh <dharmjat45@gmail.com>
2022-10-09 11:59:54 +0530

    Advance useEffect

--- src/components/HookEffectsCounter.js
+++ src/components/HookEffectsCounter.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 
 function HookEffectsCounter() {
   const [count, setCount] = useState(0);
+  const [name,setName] = useState('')
   useEffect(() => {
+    console.log('useEffect - updateing document title')
     document.title = `you Clicked ${count} times`;
   });
   return (
     <div>
+        <input type='text' value={name} onChange={e => setName(e.target.value)}></input>
       <button onClick={() => setCount(count + 1)}>Click{count}</button>
     </div>
   );
